f993dfa7959277eed760426d9e5d75c5d7be00f4
Umrzoq <umrzoq@superdispatch.com>
2023-12-10 19:34:29 +0500

    feat: divided nest js app envs and module updates on env config set up

--- apps/reservation/src/reservation.module.ts
+++ apps/reservation/src/reservation.module.ts
@@ -3,18 +3,29 @@ import { ReservationService } from './reservation.service';
 import { ReservationController } from './reservation.controller';
 import { DatabaseModule, LoggerModule } from '@app/common';
 import { ReservationRepository } from './reservation.repository';
-import { ReservationDocument, ReservationSchema } from './models/reservation.schema';
-
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 @Module({
   imports: [
     DatabaseModule,
     DatabaseModule.forFeature([
       {
         name: ReservationDocument.name,
-        schema: ReservationSchema
-      }
+        schema: ReservationSchema,
+      },
     ]),
     LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
   ],
   controllers: [ReservationController],
   providers: [ReservationService, ReservationRepository],
