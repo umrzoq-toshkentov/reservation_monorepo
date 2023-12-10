5cc9acb1cfec1c732fc593bfc4fa3256486bfd3d
Umrzoq <umrzoq@superdispatch.com>
2023-12-10 20:25:05 +0500

    feat: Implemented local auth

---
+++ apps/auth/src/strategies/local.strategy.ts
@@ -0,0 +1,28 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from '../users/users.service';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(
+    private readonly userService: UsersService,
+    private readonly configService: ConfigService,
+  ) {
+    super({
+      usernameField: 'email',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(email: string, password: string) {
+    try {
+      return await this.userService.verifyUser(email, password);
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+}
