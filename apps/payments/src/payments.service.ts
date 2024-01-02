8ec00c8e2ff5ab7ca4ee0ae568917bbbf54365f8
Umrzoq <umrzoq@superdispatch.com>
2024-01-02 14:13:01 +0500

    feat: Notifications Service emit after creation reservation

--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,11 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import Stripe from 'stripe';
-import { CreateChargeDto } from '@app/common';
+import { CreateChargeDto, NOTIFICATIONS_SERVICE } from '@app/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { PaymentCreateChargeDto } from './dto/payment-create-charge.dto';
 
 @Injectable()
 export class PaymentsService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    @Inject(NOTIFICATIONS_SERVICE)
+    private readonly notificationsService: ClientProxy,
+  ) {}
+
   private readonly stripe = new Stripe(
     this.configService.get('STRIPE_SECRET_KEY'),
     {
@@ -13,13 +20,17 @@ export class PaymentsService {
     },
   );
 
-  async createCharge({ amount }: CreateChargeDto) {
+  async createCharge({ amount, email }: PaymentCreateChargeDto) {
     const paymentIntent = await this.stripe.paymentIntents.create({
       amount: amount * 100,
       confirm: true,
       currency: 'usd',
       payment_method: 'pm_card_visa',
       return_url: 'http://localhost:3000/reservations',
+    });
+
+    this.notificationsService.emit('notify_email', {
+      email,
     });
 
     return paymentIntent;
